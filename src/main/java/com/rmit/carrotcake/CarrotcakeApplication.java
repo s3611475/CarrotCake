a2610b7f2551f131b10b3998597819c2a62c2021
dean tesoriero <s3413353@student.rmit.edu.au>
2018-04-03 20:21:03 +1000

    Adding knockout

--- src/main/java/com/rmit/carrotcake/CarrotcakeApplication.java
+++ src/main/java/com/rmit/carrotcake/CarrotcakeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarrotcakeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CarrotcakeApplication.class, args);
 	}
