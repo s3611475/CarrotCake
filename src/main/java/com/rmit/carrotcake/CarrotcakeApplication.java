7c17c18fe36f9252508ba09ea7e02a5ba49c1666
Dean Tesoriero <deant@Deans-MacBook-Pro.local>
2018-04-03 19:49:10 +1000

    Initial spring boot

---
+++ src/main/java/com/rmit/carrotcake/CarrotcakeApplication.java
@@ -0,0 +1,12 @@
+package com.rmit.carrotcake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotcakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotcakeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rmit/carrotcake/CarrotcakeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rmit.carrotcake;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarrotcakeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
